b5b53c96276486b3d3bf7f7d9b0061f68e9c999f
ffx0s <a13589949@gmail.com>
2021-03-16 15:36:56 +0800

    更换图片展示域名

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,1 +1,9 @@
-export { setImg } from '@/utils/upyun'
+// export { setImg } from '@/utils/upyun'
+export function setImg (key, options = { protocol: '' }) {
+  const prefix = 'images/'
+  if (key.startsWith(prefix)) {
+    const name = key.endsWith('jpg') ? '3' : '4'
+    key = key.split(prefix)[1]
+    return (options.protocol || '') + `//wx${name}.sinaimg.cn/large/${key}`
+  }
+}
