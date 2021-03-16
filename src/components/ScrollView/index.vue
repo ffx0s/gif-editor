4ae52933eac56f37b3bfdda4358bf90cc8a3ced6
ffx0s <a13589949@gmail.com>
2021-03-16 15:36:33 +0800

    样式调整

--- src/components/ScrollView/index.vue
+++ src/components/ScrollView/index.vue
@@ -32,9 +32,28 @@ export default {
 
 <style lang="postcss" scoped>
 .scroll-view {
+  /* 滚动条宽度 */
+  --scrollbarWidth: 5px;
+  /* 滚动条高度 */
+  --scrollbarHeight: 2px;
+  /* 滚动条颜色 */
+  --scrollbarColor: rgba(0,0,0,1);
+
   width: 100%;
   height: 100%;
   -webkit-overflow-scrolling: touch;
+  &::-webkit-scrollbar {
+    width: var(--scrollbarWidth);
+    height: var(--scrollbarHeight);
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--scrollbarColor);
+    border-radius: 10px;
+    visibility: hidden;
+  }
+  &:hover::-webkit-scrollbar-thumb {
+    visibility: visible;
+  }
   &.x {
     overflow-x: auto;
     overflow-y: hidden;
